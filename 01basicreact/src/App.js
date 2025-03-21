88b9aec526640d25cfb88398c881af8b57813931
OmkarPatil-10 <omkarpatil@gmail.com>
2025-03-21 16:55:29 +0530

    Create react projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Hello, World! | Omkar Patil </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+ 
+
+  return (
+    <h1>Hello, World! with vite | Omkar Patil </h1>
+  )
+}
+
+export default App
